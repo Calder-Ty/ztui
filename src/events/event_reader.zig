f97fc11cfd7618917fc205c366ca0b9f8ce47e9e
Tyler Calder <calder-ty@protonmail.com>
2023-11-24 14:17:46 -0700

    feat: Keep working on events

--- src/events/event_reader.zig
+++ src/events/event_reader.zig
@@ -12,8 +12,10 @@ const ReadOut = struct { [READER_BUF_SIZE]u8, usize };
 /// Read Input and generate an Event stream
 pub const EventReader = struct {
     allocator: std.mem.Allocator,
+    event_channel: std.event.Channel(keycodes.KeyCode),
 
     pub fn init(allocator: std.mem.Allocator) EventReader {
+        var event_buffer = std.RingBuffer.init(allocator, READER_BUF_SIZE * 4)
         return EventReader{ .allocator = allocator };
     }
 
@@ -25,5 +27,73 @@ pub const EventReader = struct {
         return .{ inbuff, n };
     }
 
-    fn parse_event() keycodes.KeyCode {}
+    fn name_the_func(inbuff: []u8, more: bool) {
+        var start = 0;
+        for (inbuff, 1..) |byte, i| {
+            more_available = i < inbuff.len or more;
+
+            offset = i - start;
+
+            const res = parse_event(inbuff[start..start+offset], more) catch {
+                start = i - 1;
+                continue;
+            };
+
+            if (res) 
+
+
+        }
+
+    }
+
+
+    fn parse_event(parse_buffer: []u8, more: bool) !?keycodes.KeyCode {
+        switch (parse_buffer[0]) {
+            0x1B => {
+                if (parse_buffer.len == 1) {
+                    if (more) {
+                        // Possible Esc sequence
+                        return null;
+                    } else {
+                        return keycodes.KeyCode.Esc;
+                    }
+                } else {
+                    switch (parse_buffer[1]) {
+                        'O' => {
+                            if (parse_buffer.len == 2) {
+                                return null;
+                            } else {
+                                switch (parse_buffer[2]) {
+                                    'D' => return keycodes.KeyCode.Left,
+                                    'C' => return keycodes.KeyCode.Right,
+                                    'A' => return keycodes.KeyCode.Up,
+                                    'B' => return keycodes.KeyCode.Down,
+                                    'H' => return keycodes.KeyCode.Home,
+                                    'F' => return keycodes.KeyCode.End,
+                                    // F1-F4
+                                    'P'...'S' => |val| {
+                                        return keycodes.KeyCode.F(1 + val - 'P');
+                                    },
+                                    else => return error.CouldNotParse,
+                                }
+                            }
+                        },
+                        // '[' => parse_csi(parse_buffer),
+                        '\x1B' => return keycodes.keycodes.Esc,
+                        // Not doing public events right now
+                        else => {},
+                    }
+                }
+            },
+            '\r' => return keycodes.KeyCode.Enter,
+            // '\n' We need to hanlde this.
+            '\t' => return keycodes.KeyCode.Tab,
+            0x7F => keycodes.KeyCode.Backspace,
+            0x01...0x1A => |c| keycodes.KeyCode.Char((c - 0x1 + 'a')),
+            else => {
+                // Not really, just unimplemented for now
+                unreachable;
+            },
+        }
+    }
 };
--- src/events/keyevents.zig
+++ src/events/keyevents.zig
@@ -1,10 +1,191 @@
-//! The Keyboard Events
+//! Handle Keyboard Events
+//! Much inspiration taken from Crosterm-rs
 
-pub const KeyEvent = struct {
-    code: KeyCode,
-    modifier: ?KeyModifier,
+/// Straight up lifted from crosterm-rs
+pub const KeyCodeTags = enum {
+    Backspace,
+    Enter,
+    Left,
+    Right,
+    Up,
+    Down,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+    Tab,
+    BackTab,
+    Delete,
+    Insert,
+    F,
+    Char,
+    Null,
+    Esc,
+    CapsLock,
+    ScrollLock,
+    NumLock,
+    PrintScreen,
+    Pause,
+    Menu,
+    KeypadBegin,
+    Media,
+    Modifier,
 };
 
-pub const KeyCode = enum {
-}
+pub const KeyCode = union(KeyCodeTags) {
+    /// Backspace key.
+    Backspace,
+    /// Enter key.
+    Enter,
+    /// Left arrow key.
+    Left,
+    /// Right arrow key.
+    Right,
+    /// Up arrow key.
+    Up,
+    /// Down arrow key.
+    Down,
+    /// Home key.
+    Home,
+    /// End key.
+    End,
+    /// Page up key.
+    PageUp,
+    /// Page down key.
+    PageDown,
+    /// Tab key.
+    Tab,
+    /// Shift + Tab key.
+    BackTab,
+    /// Delete key.
+    Delete,
+    /// Insert key.
+    Insert,
+    /// F key.
+    ///
+    /// `KeyCode::F(1)` represents F1 key, etc.
+    F: u8,
+    /// A character.
+    ///
+    /// `KeyCode::Char('c')` represents `c` character, etc.
+    Char: u8,
+    /// Null.
+    Null,
+    /// Escape key.
+    Esc,
+    /// Caps Lock key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    CapsLock,
+    /// Scroll Lock key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    ScrollLock,
+    /// Num Lock key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    NumLock,
+    /// Print Screen key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    PrintScreen,
+    /// Pause key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    Pause,
+    /// Menu key.
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    Menu,
+    /// The "Begin" key (often mapped to the 5 key when Num Lock is turned on).
+    ///
+    /// **Note:** this key can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    KeypadBegin,
+    /// A media key.
+    ///
+    /// **Note:** these keys can only be read if
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] has been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    Media: MediaKeyCode,
+    /// A modifier key.
+    ///
+    /// **Note:** these keys can only be read if **both**
+    /// [`KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES`] and
+    /// [`KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES`] have been enabled with
+    /// [`PushKeyboardEnhancementFlags`].
+    Modifier: ModifierKeyCode,
+};
 
+pub const MediaKeyCode = enum {
+    /// Play media key.
+    Play,
+    /// Pause media key.
+    Pause,
+    /// Play/Pause media key.
+    PlayPause,
+    /// Reverse media key.
+    Reverse,
+    /// Stop media key.
+    Stop,
+    /// Fast-forward media key.
+    FastForward,
+    /// Rewind media key.
+    Rewind,
+    /// Next-track media key.
+    TrackNext,
+    /// Previous-track media key.
+    TrackPrevious,
+    /// Record media key.
+    Record,
+    /// Lower-volume media key.
+    LowerVolume,
+    /// Raise-volume media key.
+    RaiseVolume,
+    /// Mute media key.
+    MuteVolume,
+};
+
+pub const ModifierKeyCode = enum {
+    /// Left Shift key.
+    LeftShift,
+    /// Left Control key.
+    LeftControl,
+    /// Left Alt key.
+    LeftAlt,
+    /// Left Super key.
+    LeftSuper,
+    /// Left Hyper key.
+    LeftHyper,
+    /// Left Meta key.
+    LeftMeta,
+    /// Right Shift key.
+    RightShift,
+    /// Right Control key.
+    RightControl,
+    /// Right Alt key.
+    RightAlt,
+    /// Right Super key.
+    RightSuper,
+    /// Right Hyper key.
+    RightHyper,
+    /// Right Meta key.
+    RightMeta,
+    /// Iso Level3 Shift key.
+    IsoLevel3Shift,
+    /// Iso Level5 Shift key.
+    IsoLevel5Shift,
+};

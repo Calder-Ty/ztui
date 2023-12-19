f8b31bddbf6b8f5e4be0ee6fd5a9d784bae51e6a
Tyler Calder <calder-ty@protonmail.com>
2023-12-19 13:28:13 -0700

    feat: RingBuffer to use as event Queue
    Implements a generic ring buffer

--- src/events/event_queue.zig
+++ src/events/event_queue.zig
@@ -4,52 +4,87 @@ const testing = std.testing;
 
 pub const Error = error{BufferFull};
 
-fn GenericRingBuffer(comptime T: type, comptime size: usize) type {
+fn RingBuffer(comptime T: type, comptime size: usize) type {
     return struct {
         const Self = @This();
         buffer: [size]?T,
-        start: usize,
-        end: usize,
+        start: usize = 0,
+        count: usize = 0,
 
         pub fn init() Self {
             const buff = [_]?T{null} ** size;
             return Self{
                 .buffer = buff,
-                .start = 0,
-                .end = 0,
             };
         }
 
         /// Push an Item of Type T onto the buffer
         pub fn push(self: *Self, item: T) !void {
-            const next_i = self.next_index();
-            // Check Buffer is not full
-            if (self.start == next_i) {
-                return Error.BufferFull;
-            }
+            const next_i = try self.next_index();
             self.buffer[next_i] = item;
-            self.end += 1;
+            self.count += 1;
         }
 
-        fn next_index(self: *Self) usize {
-            if (self.end == size - 1) {
-                return 0;
+        /// Generate the Next Index. Does bounds checking and wraps around
+        fn next_index(self: *Self) !usize {
+            var next_val: usize = undefined;
+            if (self.count + 1 > size) {
+                return error.BufferFull;
             }
-            return self.end + 1;
+            // Loop around
+            if (self.start + self.count >= size) {
+                next_val = self.start + self.count - size;
+            } else {
+                next_val = self.start + self.count;
+            }
+            return next_val;
         }
 
         /// Pop an Item out an optional T
-        pub fn pop(self: *Self) !?T {
-            if (self.end == self.start) {
+        pub fn pop(self: *Self) ?T {
+            if (self.count == 0) {
                 // Empty
                 return null;
             }
+
+            const res = self.buffer[self.start];
+            if (self.start == size) {
+                self.*.start = 0;
+            } else {
+                self.*.start += 1;
+            }
+            self.count -= 1;
+            return res;
         }
     };
 }
 
 test "Push value" {
-    var test_rb = GenericRingBuffer(u8, 8).init();
+    var test_rb = RingBuffer(u8, 8).init();
     try test_rb.push(255);
     try testing.expect(test_rb.buffer[0] == 255);
+}
+
+test "Pop value" {
+    var test_rb = RingBuffer(u8, 8).init();
+    try test_rb.push(255);
+    _ = test_rb.pop();
+    try testing.expect(test_rb.start == 1);
+}
+
+test "Push/Pop many values" {
+    var test_rb = RingBuffer(u8, 8).init();
+    try test_rb.push(255);
+    try test_rb.push(255);
+    _ = test_rb.pop();
+    _ = test_rb.pop();
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try test_rb.push(255);
+    try testing.expect(test_rb.start == 2);
+    try testing.expect(try test_rb.next_index() == 1);
 }
--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,13 @@ const std = @import("std");
 const testing = std.testing;
 pub const terminal = @import("./terminal.zig");
 pub const EventReader = @import("./events/event_reader.zig").EventReader;
+pub const event_queue = @import("./events/event_queue.zig");
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
 test "basic add functionality" {
+    testing.refAllDecls(@This());
     try testing.expect(add(3, 7) == 10);
 }

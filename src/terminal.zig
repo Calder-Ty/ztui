93877f5a90998c7858c9a514ef7821448d74cded
Tyler Calder <calder-ty@protonmail.com>
2024-03-10 20:14:55 -0600

    feat: Remove Terminal hold on tty file handle
    Originally I wanted the Terminal Struct to hold onto the File Handle
    for /dev/tty, but it is not neccessary, and it actively gets in the
    way of being able to do things like detect Progressive enahancemnet
    support.

--- src/terminal.zig
+++ src/terminal.zig
@@ -2,73 +2,76 @@
 //!
 const std = @import("std");
 const fs = std.fs;
+const Mutex = std.Thread.Mutex;
+const Terminal = @This();
 
 const c = @cImport(@cInclude("termios.h"));
 
 const TerminalErrors = error{ ClearScreenFailure, GetTermiosAttrError, SetTermiosAttrError };
 
-pub const Terminal = struct {
-    fh: fs.File,
-    // TODO: What if we want to do this in a threaded environment? Let's make this threadsafe
-    orig_termios: ?c.termios = null,
+const OrigTermiosMutex = struct {
+    mutex: Mutex = Mutex{},
+    _orig_termios: ?c.termios = null,
 
-    pub fn init() !@This() {
-        // Get Control of Terminal
-        const fh = try fs.openFileAbsolute("/dev/tty", .{
-            .mode = .read_write,
-            .allow_ctty = true,
-        });
-        // Clear Screen
-        // FIXME: Handle Errors
-        try alt_screen(&fh);
-        errdefer orig_screen(&fh);
-        var term: Terminal = Terminal{ .fh = fh };
-        try term.enable_raw_mode();
-        return term;
+    inline fn lock(self: *OrigTermiosMutex) *?c.termios {
+        self.mutex.lock();
+        return &self._orig_termios;
     }
 
-    pub fn deinit(self: Terminal) void {
-        // Reset Screen
-        self.disable_raw_mode() catch {
-            //XXX: Handle this properly?
-        };
-        orig_screen(&self.fh);
-        self.fh.close();
-    }
-
-    /// Put the Terminal into "Raw" mode where key input isn't sent to Screen
-    /// and line breaks aren't handled. See crosterm-rs and `man termios` for more info
-    fn enable_raw_mode(self: *Terminal) !void {
-        if (self.orig_termios) |_| {
-            return;
-        }
-
-        var ios: c.termios = undefined;
-        try wrap_as_error_union(c.tcgetattr(self.fh.handle, &ios), TerminalErrors.GetTermiosAttrError);
-        const orig = ios;
-
-        c.cfmakeraw(&ios);
-        try wrap_as_error_union(c.tcsetattr(self.fh.handle, c.TCSANOW, &ios), TerminalErrors.SetTermiosAttrError);
-
-        self.orig_termios = orig;
-    }
-
-    fn disable_raw_mode(self: *const Terminal) !void {
-        if (self.orig_termios) |orig_ios| {
-            try wrap_as_error_union(c.tcsetattr(self.fh.handle, c.TCSANOW, &orig_ios), TerminalErrors.SetTermiosAttrError);
-        }
+    inline fn unlock(self: *OrigTermiosMutex) void {
+        self.mutex.unlock();
     }
 };
 
-fn alt_screen(fh: *const fs.File) !void {
+var orig_termios_mutex = OrigTermiosMutex{};
+
+/// Put the Terminal into "Raw" mode where key input isn't sent to Screen
+/// and line breaks aren't handled. See crosterm-rs and `man termios` for more info
+pub fn enableRawMode(self: *Terminal) !void {
+    const orig_termios = orig_termios_mutex.lock();
+    defer orig_termios_mutex.unlock();
+    if (orig_termios.*) |_| {
+        return;
+    }
+
+    var ios: c.termios = undefined;
+    try wrapAsErrorUnion(c.tcgetattr(self.fh.handle, &ios), TerminalErrors.GetTermiosAttrError);
+    const orig = ios;
+
+    c.cfmakeraw(&ios);
+    try wrapAsErrorUnion(c.tcsetattr(self.fh.handle, c.TCSANOW, &ios), TerminalErrors.SetTermiosAttrError);
+
+    // Set the orig_termios if needed
+    orig_termios.* = orig;
+}
+
+/// Release Terminal from "Raw" mode. See `man termios` for more information on
+/// RawMode
+pub fn disableRawMode(self: *const Terminal) !void {
+    const orig_termios = orig_termios_mutex.lock();
+    defer orig_termios_mutex.unlock();
+    if (orig_termios.*) |orig_ios| {
+        try wrapAsErrorUnion(c.tcsetattr(self.fh.handle, c.TCSANOW, &orig_ios), TerminalErrors.SetTermiosAttrError);
+    }
+}
+
+pub fn altScreen() !void {
+    const fh = try fs.openFileAbsolute("/dev/tty", .{
+        .mode = .read_write,
+        .allow_ctty = true,
+    });
     _ = try fh.write("\x1B[?1049h");
 }
 
-fn orig_screen(fh: *const fs.File) void {
+pub fn origScreen() void {
+    const fh = try fs.openFileAbsolute("/dev/tty", .{
+        .mode = .read_write,
+        .allow_ctty = true,
+    });
     _ = fh.write("\x1B[?1049l") catch {};
 }
 
-fn wrap_as_error_union(return_no: i32, comptime ERROR_VARIANT: TerminalErrors) !void {
+fn wrapAsErrorUnion(return_no: i32, comptime ERROR_VARIANT: TerminalErrors) !void {
     if (return_no == -1) {
         return ERROR_VARIANT;
     }

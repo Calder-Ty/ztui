6a2bef0a91b86b1ed6bd524591dcf2a420358da4
Tyler Calder <calder-ty@protonmail.com>
2024-03-14 11:17:28 -0600

    feat: Make EventReader internal

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
 pub const terminal = @import("./terminal.zig");
-pub const EventReader = @import("./events/event_reader.zig").EventReader;
 pub const event_queue = @import("./events/event_queue.zig");
 
 export fn add(a: i32, b: i32) i32 {

05069723e4040d69e12210f8748afba96db04edb
Tyler Calder <calder-ty@protonmail.com>
2024-04-08 21:34:29 -0600

    feat: remove event_queue from public api

--- ztui-tabby/root.zig
+++ ztui-tabby/root.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
 pub const terminal = @import("terminal.zig");
-pub const event_queue = @import("events/event_queue.zig");
 pub const event_reader = @import("events/event_reader.zig");
 
 test "basic add functionality" {
